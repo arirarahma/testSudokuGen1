1d8738dc29bcec0d8fdc0d35c2c761364a0ef66b
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 01:05:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-import { hello1, hello2} from 'square.js';
-import defaultFunction from 'square.js';
+import {user} from './user.js';
 
-let output1 = hello1();
-
-let output2 = hello2();
-
-let output3 = 'Output3';
-
-let outputDefault = defaultFunction();
-
-document.getElementById('default').innerHTML = 'Test test test html default' ;
-document.getElementById('demo').innerHTML = output3;
+document.body.innerHTML = user; // John
