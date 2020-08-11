5c577f959a3f9cef17e534dd706d077f34657583
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 00:55:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-import { create, createReportList } from './modules/canvas.js';
-import { name, draw, reportArea, reportPerimeter } from './modules/square.js';
-import randomSquare from './modules/square.js';
+import { hello1, hello2} from 'square.js';
+import defaultFunction from 'square.js';
 
-let myCanvas = create('myCanvas', document.body, 480, 320);
-let reportList = createReportList(myCanvas.id);
+let output1 = hello1();
 
-let square1 = draw(myCanvas.ctx, 50, 50, 100, 'blue');
-reportArea(square1.length, reportList);
-reportPerimeter(square1.length, reportList);
+let output2 = hello2();
 
-// Use the default
-let square2 = randomSquare(myCanvas.ctx);
+let outputDefault = defaultFunction();
