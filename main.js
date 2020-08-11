a95e42a7a77f9bc86a7eab4c17e780c86494276f
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 01:22:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {user} from './modules/user.js';
 
-let user;
+let var1 = 'variable 1';
 
-document.body.innerHTML = 'hahahhah' + user; // John
+document.body.innerHTML = 'hahahhah' + var1; // John
