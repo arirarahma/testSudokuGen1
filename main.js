2a4a74422ac893e45c1c09ce973de64394b7d61b
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 01:04:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,6 @@ let output2 = hello2();
 let output3 = 'Output3';
 
 let outputDefault = defaultFunction();
+
+document.getElementById('default').innerHTML = 'Test test test html default' ;
+document.getElementById('demo').innerHTML = output3;
