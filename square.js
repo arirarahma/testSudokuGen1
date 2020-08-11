b6c6288cc407fcfaf90dbf80a832c53db9eec1c4
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 01:05:18 +0700

    Update square.js

--- square.js
+++ square.js
@@ -1,14 +1,1 @@
-function hello1() {
-  return 'Test function 1;
-}
-
-function hello2() {
-  return 'Test function 2;
-}
-
-function defaultFunction() {
-  return 'Test function Default;
-}
-
-export { hello1, hello2};
-export default defaultFunction;
+export let user = "John";
