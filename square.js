692fc086aac9f7b30c9878b82a1c8cd7cd66f80f
arirarahma <37467118+arirarahma@users.noreply.github.com>
2020-08-12 00:53:10 +0700

    Update square.js

--- square.js
+++ square.js
@@ -1,57 +1,14 @@
-const name = 'square';
-
-function draw(ctx, length, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, length, length);
-
-  return {
-    length: length,
-    x: x,
-    y: y,
-    color: color
-  };
+function hello1() {
+  return 'Test function 1;
 }
 
-function random(min, max) {
-   let num = Math.floor(Math.random() * (max - min)) + min;
-   return num;
+function hello2() {
+  return 'Test function 2;
 }
 
-function reportArea(length, listId) {
-  let listItem = document.createElement('li');
-  listItem.textContent = `${name} area is ${length * length}px squared.`
-
-  let list = document.getElementById(listId);
-  list.appendChild(listItem);
+function defaultFunction() {
+  return 'Test function 1;
 }
 
-function reportPerimeter(length, listId) {
-  let listItem = document.createElement('li');
-  listItem.textContent = `${name} perimeter is ${length * 4}px.`
-
-  let list = document.getElementById(listId);
-  list.appendChild(listItem);
-}
-
-function randomSquare(ctx) {
-  let color1 = random(0, 255);
-  let color2 = random(0, 255);
-  let color3 = random(0, 255);
-  let color = `rgb(${color1},${color2},${color3})`
-  ctx.fillStyle = color;
-
-  let x = random(0, 480);
-  let y = random(0, 320);
-  let length = random(10, 100);
-  ctx.fillRect(x, y, length, length);
-
-  return {
-    length: length,
-    x: x,
-    y: y,
-    color: color
-  };
-}
-
-export { name, draw, reportArea, reportPerimeter };
-export default randomSquare;
+export { hello1, hello2};
+export default defaultFunction;
